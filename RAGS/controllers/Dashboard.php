f14a2b1f79f740d4ae5b248f36d93ec2d43e97df
Juanes79 <jhernandez15007@gmail.com>
2024-12-07 15:23:31 -0500

    modificaciones del dashboard

--- RAGS/controllers/Dashboard.php
+++ RAGS/controllers/Dashboard.php
@@ -1,7 +1,10 @@
 <?php
     class Dashboard{        
-        public function main(){            
-            require_once "views/roles/admin/index.view.php";                        
-        }
+        public function main(){ 
+            require_once "views/roles/admin/header.view.php";  
+            require_once "views/roles/admin/index.view.php";  
+            require_once "views/roles/admin/footer.view.php";  
+
+         }
     }
 ?>
